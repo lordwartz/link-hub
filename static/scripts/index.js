1ce1ce3d637acf1b653cda8c3c417e6d786f4525
NeonyFenyx <idrishal277@gmail.com>
2024-05-15 01:17:45 +0500

    implement link creation through server

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,41 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
     addItemListenerToContainer('main_groups')
     addItemListenerToContainer('quick_menu');
-    let quick_group_template = document.querySelector('#quick_menu_group_template');
-    let main_group_template = document.querySelector('#main_group_template');
-    let add_button_template = document.querySelector('#add_group');
-    for (let i = 1; i <= 12; i++) {
-        {
-            let clone = document.importNode(main_group_template.content, true);
-            let link = clone.querySelector('a');
-            link.textContent = `Группа ${i}`;
-            link.href = `#group${i}`;
-            document.querySelector('.main_groups').appendChild(clone);
-        }
-        {
-            let clone = document.importNode(quick_group_template.content, true);
-            let link = clone.querySelector('a');
-            link.textContent = `Группа ${i}`;
-            link.href = `#group${i}`;
-            if (i === 3) {
-                link.classList.add('active_quick_menu_group');
+    document.querySelector('.add_main_group_link').addEventListener('click', () => {
+        const formResult = askForm([
+            {
+                type: "text",
+                name: "name",
+                placeholder: "Введите имя группы",
+                label: "Имя"
             }
-            document.querySelector('.quick_menu').appendChild(clone);
-        }
-    }
+        ], "Создать группу", "Отменить");
 
-    let clone = document.importNode(add_button_template.content, true);
-    document.querySelector('.main_groups').appendChild(clone);
-
-    let quickLinks = document.querySelectorAll('.quick_menu a');
-
-    quickLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            quickLinks.forEach(link => {
-                link.classList.remove('active_quick_menu_group');
+        formResult
+            .then(groupData => {
+                const cardCount = document.querySelector('.main_groups').children.length - 1;
+                return fetch('/add', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        parent: 'root',
+                        content: {
+                            name: groupData.name,
+                            order: cardCount
+                        }
+                    })
+                });
+            })
+            .then(() => {
+                location.reload();
             });
-            this.classList.add('active_quick_menu_group');
-        });
     });
-})
+});
