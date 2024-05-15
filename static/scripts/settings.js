97c14c86efc6d83a452f1b161a8449ce5ec85853
lordwartz <Lwartz@yandex.ru>
2024-05-16 02:32:02 +0500

    rework content header display positions

--- static/scripts/settings.js
+++ static/scripts/settings.js
@@ -4,16 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const itemContainer = document.querySelector('.quick_menu');
     let editProfile = document.querySelector('#edit_profile');
     let securitySettings = document.querySelector('#security_settings');
+    const contentHeader = document.querySelector('.content_header span');
 
     itemContainer.addEventListener('click', function(event) {
         const group = event.target.closest('.group');
         const url = group.dataset.url;
         if (url.includes('edit')) {
             securitySettings.style.display = 'none';
-            editProfile.style.display = 'flex'
+            editProfile.style.display = 'flex';
+            contentHeader.textContent = 'Редактирование профиля';
         } else {
             editProfile.style.display = 'none';
             securitySettings.style.display = 'flex';
+            contentHeader.textContent = 'Безопасность';
         }
     });
 
